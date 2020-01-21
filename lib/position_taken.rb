6d3a99c76c6ca2e3f84ce9b3a7baa418911dc566
Bryant Omoregie <unclebryant@gmail.com>
2020-01-21 21:32:17 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,9 @@
 def position_taken?(board, index)
-  board[index] != " "
-  
+  if board[index] == "x" || "o"
+    return true
+  else return false
+  end
+
 end
 
   # code your #position_taken? method here!
