7a07113fa3343886678f56602c8b028f9c02fdc9
Bryant Omoregie <unclebryant@gmail.com>
2020-01-21 22:16:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 def position_taken?(board, index)
- if board[index] == " " || board[index] ==  "" || board[index] ==  nil
-   return false
+ if board[index] != " " || board[index] !=  "" || board[index] !=  nil
+   
  else
    return true
   end
